0320df5954b22357ff9d0787219227c35f5fbbc0
hien.le <hien.le@ericsson.com>
2019-12-08 13:04:08 -0500

    Updates

---
+++ precache-manifest.6facf5bd88679ee6040c618ebb265d7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf66f3a9ad1228a910d28a55dc5a5e70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99d8532ab394bca8dc8e",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "944590533b28494b0213",
+    "url": "/static/js/2.0c9ca595.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0c9ca595.chunk.js.LICENSE"
+  },
+  {
+    "revision": "99d8532ab394bca8dc8e",
+    "url": "/static/js/main.cb600c5b.chunk.js"
+  },
+  {
+    "revision": "1d51611349a237301b34",
+    "url": "/static/js/runtime-main.08cabbc3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
